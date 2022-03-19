3bb7c0f208551362a9f061c035544aec23e355eb
Mohamed Darwesh <midodarwesh285@gmail.com>
2022-03-19 23:09:16 +0200

    header component

---
+++ Client/src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../css/Header/Header.css'
+
+function Header() {
+  return (
+    <>
+    <nav className='nav-bar'>
+    <h1>E-Commerce</h1>
+    </nav>
+    </>
+  )
+}
+
+export default Header

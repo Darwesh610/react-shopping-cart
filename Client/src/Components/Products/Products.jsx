2bf6d7ecf78fb74f2c5d89a31ac1ad81276b8943
Mohamed Darwesh <midodarwesh285@gmail.com>
2022-03-20 13:38:15 +0200

    product-details

--- Client/src/Components/Products/Products.jsx
+++ Client/src/Components/Products/Products.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../css/Products/Products.css'
+import Modal from 'react-modal'
 
 function Products(props) {
-    console.log(props.data);
+    const [product, setProduct] = useState("")
+
+    const openModal = (product) => {
+        setProduct(product)
+    }
+    const closeModel = () => {
+        setProduct('')
+    }
 return (
     <div className='products'>
     {props.data.map((product , indx) => <div className='product' key={indx}>
-        <img src={product.image} alt={product.title} />
+        <a href="##" onClick={() => openModal(product)}>
+        <img className='image' src={product.image} alt={product.title} />
+        </a>
         <h3>{product.title}</h3>
         <p>{product.desc}</p>
         <span>{product.price}</span>
@@ -16,6 +26,18 @@ return (
         <button className='butt'>Add to Cart</button>
         </div>
     )}
+
+    <Modal isOpen={product} onRequestClose={closeModel}>
+        <span className='close' onClick={closeModel}>&times; </span>
+        <div className="product-info">
+        <img src={product.image} alt={product.title} />
+        <h3>{product.title}</h3>
+        <p>{product.desc}</p>
+        <span>{product.price}</span>
+        <br />
+        <span>{product.sizes}</span>
+        </div>
+    </Modal>
     </div>
 )
 }

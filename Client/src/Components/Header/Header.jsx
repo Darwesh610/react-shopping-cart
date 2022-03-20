78e9a10fb31d2b531f208f930120b2fed05174ec
Mohamed Darwesh <midodarwesh285@gmail.com>
2022-03-20 11:47:01 +0200

    add data json | words data

--- Client/src/Components/Header/Header.jsx
+++ Client/src/Components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../../css/Header/Header.css'
+import { words } from '../../words'
 
 function Header() {
   return (
     <>
     <nav className='nav-bar'>
-    <h1>E-Commerce</h1>
+    <h1>{words.headerTitle}</h1>
     </nav>
     </>
   )
